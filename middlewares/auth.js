b6bf965c536581f4fc1998627dff07e83c1d7128
Starikova Maria <maria050200@gmail.com>
2024-01-19 17:25:29 +0500

    ПР 14

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -6,7 +6,9 @@ module.exports = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
-    throw new UnauthorizedError("Необходима авторизация");
+    // throw new UnauthorizedError("Необходима авторизация");
+    const unauthorizedError = new UnauthorizedError("Необходима авторизация");
+    res.status(unauthorizedError.statusCode).send({ message: unauthorizedError.message });
   }
 
   const token = authorization.replace("Bearer ", "");
@@ -16,7 +18,9 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, "some-secret-key");
   } catch (err) {
-    throw new UnauthorizedError("Необходима авторизация");
+    // throw new UnauthorizedError("Необходима авторизация");
+    const unauthorizedError = new UnauthorizedError("Необходима авторизация");
+    res.status(unauthorizedError.statusCode).send({ message: unauthorizedError.message });
   }
 
   req.user = payload;

9a0687410bef9dbabfdedf1d02c9cb0c33995856
Starikova Maria <maria050200@gmail.com>
2024-01-24 19:39:56 +0500

    ПР 14

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const routers = require("./routes/index");
 const handlerErrors = require("./middlewares/handlerErrors");
 const { errors } = require("celebrate");
+const { requestLogger, errorLogger } = require("./middlewares/logger");
 
  const { PORT = 3000 } = process.env;
 
@@ -11,19 +12,24 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(requestLogger);
+
 app.use(routers);
+
+app.use(errorLogger);
 
 app.use(errors());
 app.use(handlerErrors);
 
 mongoose.connect("mongodb://localhost:27017/mestodb", {
-  useNewUrlParser: true,
+  // useNewUrlParser: true,
+  useUnifiedTopology: true,
 }).then(() => {
   console.log("'соединение с базой установлено");
 })
   .catch(() => {
     console.log("'соединение с базой прервано");
-    process.exit(1);
+    // process.exit(1);
   });
 
 app.listen(PORT, () => {

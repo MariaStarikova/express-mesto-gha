de5463687a443912ace2242917a01c2fcd6b42b7
Starikova Maria <maria050200@gmail.com>
2024-01-26 20:46:46 +0500

    GGGG

--- app.js
+++ app.js
@@ -36,6 +36,7 @@ mongoose.connect("mongodb://127.0.0.1:27017/mestodb", {
 
 app.listen(PORT, () => {
   // Если всё работает, консоль покажет, какой порт приложение слушает
+  console.log("V7");
   console.log(`App listening on port ${PORT}`);
 });
 

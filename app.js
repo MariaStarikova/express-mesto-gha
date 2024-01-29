837835e8932791f77346709d40076aa99883055b
Starikova Maria <maria050200@gmail.com>
2024-01-29 16:38:43 +0500

    CORS

--- app.js
+++ app.js
@@ -43,6 +43,7 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
     // разрешаем кросс-доменные запросы с этими заголовками
     res.header("Access-Control-Allow-Headers", requestHeaders);
+    console.log("проверка CORS");
     // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }

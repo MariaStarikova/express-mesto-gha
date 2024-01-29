f6252a524ccfb160ef7e618e905d89c9a5618eb3
Starikova Maria <maria050200@gmail.com>
2024-01-29 18:04:50 +0500

    CORS

--- app.js
+++ app.js
@@ -25,40 +25,6 @@ app.options("*", cors(allowedCors));
 app.use(cors(allowedCors));
 app.use(helmet());
 
-// app.use(function(req, res, next) {
-//   const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
-//   // проверяем, что источник запроса есть среди разрешённых
-
-//   if (allowedCors.includes(origin)) {
-//     res.header("Access-Control-Allow-Origin", origin);
-//   }
-
-//   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
-
-//   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
-//   const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
-
-//   // сохраняем список заголовков исходного запроса
-//   const requestHeaders = req.headers["access-control-request-headers"];
-
-
-
-//   // Если это предварительный запрос, добавляем нужные заголовки
-//   if (method === "OPTIONS") {
-//     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
-//     res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
-//     // разрешаем кросс-доменные запросы с этими заголовками
-//     res.header("Access-Control-Allow-Headers", requestHeaders);
-//     console.log("проверка CORS");
-//     // завершаем обработку запроса и возвращаем результат клиенту
-//     return res.end();
-//   }
-
-//   // res.header("Access-Control-Allow-Origin", "*");
-
-//   return next();
-// });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 

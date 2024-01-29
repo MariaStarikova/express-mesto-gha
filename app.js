43a9df074e1ec3bc4404f0aa07b5559f972e4dd2
Starikova Maria <maria050200@gmail.com>
2024-01-29 15:55:47 +0500

    CORS

--- app.js
+++ app.js
@@ -5,9 +5,52 @@ const handlerErrors = require("./middlewares/handlerErrors");
 const { errors } = require("celebrate");
 const { requestLogger, errorLogger } = require("./middlewares/logger");
 
+
  const { PORT = 3000 } = process.env;
 
 const app = express();
+
+const allowedCors = [
+  "https://mstarikova.students.nomoredomainsmonster.ru",
+  "http://mstarikova.students.nomoredomainsmonster.ru",
+  "http://api.mstarikova.students.nomoredomainsmonster.ru",
+  "https://api.mstarikova.students.nomoredomainsmonster.ru",
+  "http://localhost:3000",
+  "https://localhost:3000"
+];
+
+app.use(function(req, res, next) {
+  const { origin } = req.headers; // Сохраняем источник запроса в переменную origin
+  // проверяем, что источник запроса есть среди разрешённых
+
+  if (allowedCors.includes(origin)) {
+    res.header("Access-Control-Allow-Origin", origin);
+  }
+
+  const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
+
+  // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
+  const DEFAULT_ALLOWED_METHODS = "GET,HEAD,PUT,PATCH,POST,DELETE";
+
+  // сохраняем список заголовков исходного запроса
+  const requestHeaders = req.headers["access-control-request-headers"];
+
+
+
+  // Если это предварительный запрос, добавляем нужные заголовки
+  if (method === "OPTIONS") {
+    // разрешаем кросс-доменные запросы любых типов (по умолчанию)
+    res.header("Access-Control-Allow-Methods", DEFAULT_ALLOWED_METHODS);
+    // разрешаем кросс-доменные запросы с этими заголовками
+    res.header("Access-Control-Allow-Headers", requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
+    return res.end();
+  }
+
+  // res.header("Access-Control-Allow-Origin", "*");
+
+  return next();
+});
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

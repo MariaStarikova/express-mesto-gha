00a0b91bbd9c9bf3041304469e257f0318431962
Starikova Maria <maria050200@gmail.com>
2024-01-30 15:23:01 +0500

    Token

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -4,7 +4,6 @@ const UnauthorizedError = require("../errors/unauthorized-err");
 module.exports = (req, res, next) => {
   const { authorization } = req.headers;
   console.log("authorization", authorization);
-  console.log("authorization.startsWith(Bearer )", authorization.startsWith("Bearer "));
 
   if (!authorization || !authorization.startsWith("Bearer ")) {
     throw new UnauthorizedError("Необходима авторизация");
